df0b2b9613dcb3be78297d44c9123aa1ea76700c
antitoxic <antitoxic@gmail.com>
2014-06-14 10:13:13 +0300

    Handle tabs' layout on users page via isotope plugin

--- projects/static/js/users.js
+++ projects/static/js/users.js
@@ -1,5 +1,11 @@
-$(function() {
+$(function () {
     var $skills = $('#joinSkills');
     new Select2Grouped($skills, $skills.data('choices'))
+
+    var $container = $('.nav-tabs ul').isotope({
+        itemSelector: 'li',
+        layoutMode: 'fitRows',
+        isOriginTop: false
+    });
 })
 
