c2ed494cfd39ce98b3187eaa39a91caa5e70fe74
Mihail Mikov <m.mikoff@gmail.com>
2014-07-18 08:45:56 +0300

    working on the upload form location

--- projects/static/js/profile.js
+++ projects/static/js/profile.js
@@ -1,4 +1,23 @@
-$(function() {
-    var $skills = $('#joinSkills');
-    new Select2Grouped($skills, $skills.data('choices'), $skills.data('selection'))
-})
+$(function () {
+    var $skills = $('#joinSkills'),
+        avatarPicker = $('#avatar'),
+        avatarPrev = $('#prev_avatar'),
+        location = $('#location'),
+
+    new Select2Grouped($skills, $skills.data('choices'), $skills.data('selection'));
+
+    avatarPrev.on('click', function () {
+        avatarPicker.click();
+    });
+
+    avatarPrev.on('load', function () {
+        window.URL.revokeObjectURL(avatarPrev.attr('src'));
+    });
+
+    avatarPicker.on('change', function () {
+        var imgUrl = window.URL.createObjectURL(avatarPicker[0].files[0]);
+        console.log(imgUrl);
+        avatarPrev.attr('src', imgUrl);
+    });
+
+});
