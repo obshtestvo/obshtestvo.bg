2dfc2f6e82e3c00ca6dcda875456364ac409b7ca
Anton Stoychev <antitoxic@gmail.com>
2014-08-02 15:54:02 +0300

    Disable restful error handler that is still in development and causes issues.

--- settings.py
+++ settings.py
@@ -120,7 +120,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'restful.error_handler.ErrorHandler',
+    # 'restful.error_handler.ErrorHandler',
     'restful.middleware.ResponseFormatDetection',
 )
 
